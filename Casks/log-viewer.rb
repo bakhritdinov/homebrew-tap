cac0e2a32a82f3037d2b991480020f2debfd2ac1
GitHub Action <action@github.com>
2026-05-08 08:14:26 +0000

    Update LogViewer Cask to v2.2.1

--- Casks/log-viewer.rb
+++ Casks/log-viewer.rb
@@ -1,6 +1,6 @@
 cask "log-viewer" do
-  version "2.2.0"
-  sha256 "bfee5c42e0a1dab73231c6c2787ce85084fb4927fdd476b579c3febd38d272b0"
+  version "2.2.1"
+  sha256 "4e971214488a6deefc858326640ffef40d289b0db6ec342a9f9e2dc31c1b88cd"
 
   url "https://github.com/bakhritdinov/log-viewer/releases/download/v#{version}/LogViewer-macOS-Universal.dmg"
   name "LogViewer"
